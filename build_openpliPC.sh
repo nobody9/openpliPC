270f26daab556a98adfb7f37a6ff00762bf1ab38
ambrosa <aleambro@gmail.com>
2011-12-17 17:20:54 +0100

    added -b -r command line options

--- build_openpliPC.sh
+++ build_openpliPC.sh
@@ -3,9 +3,45 @@
 # where install Enigma2 tree
 INSTALL_E2DIR="/usr/local/e2"
 
-#define enigma2 ref and enigma2 patch
-###REF="40eea73ce3269c031d231e8cf4ef307cfbc1466c"
-###PATCH="openpliPC_20111216.patch"
+BACKUP_E2="etc/enigma2 etc/tuxbox/*.xml etc/tuxbox/nim_sockets"
+
+# ----------------------------------------------------------------------
+
+DO_BACKUP=0
+DO_RESTORE=0
+
+function e2_backup {
+        echo "-----------------------------"
+        echo "BACKUP E2 CONFIG"
+        echo "-----------------------------"
+
+	tar -C $INSTALL_E2DIR -v -c -z -f e2backup.tgz $BACKUP_E2
+}
+
+function e2_restore {
+        echo "-----------------------------"
+        echo "RESTORE OLD E2 CONFIG"
+        echo "-----------------------------"
+        
+	if [ -f e2backup.tgz ]; then
+		sudo tar -C $INSTALL_E2DIR -v -x -z -f e2backup.tgz
+	fi
+}
+
+
+while [ "$1" != "" ]; do
+    case $1 in
+        -b )  DO_BACKUP=1
+              ;;
+        -r )  DO_RESTORE=1
+              ;;
+    esac
+    shift
+done
+
+if [ "$DO_BACKUP" -eq "1" ]; then
+	e2_backup
+fi
 
 # ----------------------------------------------------------------------
 
@@ -106,6 +142,10 @@ sudo cp share/fonts/* $INSTALL_E2DIR/share/fonts
 sudo cp -rf etc/* $INSTALL_E2DIR/etc
 
 ln -sf $INSTALL_E2DIR/bin/enigma2 ./e2bin
+
+if [ "$DO_RESTORE" -eq "1" ]; then
+	e2_restore
+fi
 
 echo ""
 echo "**********************<END>**********************"
