2d70c7e859017a8d35516d504930a63e653d9c9d
ambrosa <aleambro@gmail.com>
2011-12-17 14:34:27 +0100

    Update build_enigma2.sh

--- build_enigma2.sh
+++ build_enigma2.sh
@@ -1,19 +1,60 @@
 #!/bin/sh
 
 # where install Enigma2 tree
-INSTALL_DIR = "/usr/local/e2"
+INSTALL_E2DIR = "/usr/local/e2"
 
 #define enigma2 ref and enigma2 patch
 ###REF="40eea73ce3269c031d231e8cf4ef307cfbc1466c"
 ###PATCH="openpliPC_20111216.patch"
 
-#Build and install enigma2:
+# ----------------------------------------------------------------------
+
+# Build and install xine-lib:
+PKG="xine-lib"
+echo "-----------------------------------------"
+echo "configuring OpenPliPC $PKG"
+echo "-----------------------------------------"
+
+#if [ -d $PKG ]; then
+#	echo "Erasing older build dir"
+#	rm -Rf $PKG
+#	rm -f $PKG*
+#fi
+#git clone git://projects.vdr-developer.org/$PKG.git
+
+cd $PKG
+
+#git checkout df-osd-handling+alter-vdpau-h264-decoder
+#git checkout $XINELIB_REF
+#patch -p1 < ../patches/$XINELIB_PATCH
+
+./autogen.sh --disable-xinerama --disable-musepack --prefix=/usr
+
+echo "-----------------------------------------"
+echo "build OpenPliPC $PKG, please wait..."
+echo "-----------------------------------------"
+
+make
+
+echo "--------------------------------------"
+echo "installing OpenPliPC $PKG"
+echo "--------------------------------------"
+
+sudo make install
+cd ..
+
+# ----------------------------------------------------------------------
+
+# Build and install enigma2:
+
 #echo "--------------------------------------"
 #echo "downloading OpenPli sources, please wait..."
 #echo "--------------------------------------"
-#PKG="enigma2"
+
+PKG="enigma2"
+
 #if [ -d $PKG ]; then
-#	echo "Erasing older build dir"
+# echo "Erasing older build dir"
 #	rm -Rf $PKG
 #	rm -f $PKG*
 #fi
@@ -23,70 +64,46 @@ INSTALL_DIR = "/usr/local/e2"
 #echo "downloading OpenPli sources, please wait..."
 #echo "--------------------------------------"
 
-#Build and install xine-lib:
-PKG="xine-lib"
-echo "-----------------------------------------"
+echo "--------------------------------------"
 echo "configuring OpenPliPC $PKG"
-echo "-----------------------------------------"
-#if [ -d $PKG ]; then
-#	echo "Erasing older build dir"
-#	rm -Rf $PKG
-#	rm -f $PKG*
-#fi
-#git clone git://projects.vdr-developer.org/$PKG.git
+echo "--------------------------------------"
+
 cd $PKG
-#git checkout df-osd-handling+alter-vdpau-h264-decoder
-#git checkout $XINELIB_REF
-#patch -p1 < ../patches/$XINELIB_PATCH
-./autogen.sh --disable-xinerama --disable-musepack --prefix=/usr
-echo "-----------------------------------------"
-echo "build OpenPliPC $PKG, please wait..."
-echo "-----------------------------------------"
-make
-echo "--------------------------------------"
-echo "installing OpenPliPC $PKG"
-echo "--------------------------------------"
 
-sudo make install
-cd ..
-
-cd enigma2
 #git checkout $REF
- 
-echo "--------------------------------------"
-echo "configuring OpenPli sources"
-echo "--------------------------------------"
 #patch -p1 < ../patches/$PATCH
 
 autoreconf -i
-./configure --prefix=$INSTALL_DIR --with-xlib --with-debug
+./configure --prefix=$INSTALL_E2DIR --with-xlib --with-debug
  
 echo "--------------------------------------"
-echo "build OpenPli, please wait..."
+echo "build OpenPliPC $PKG, please wait..."
 echo "--------------------------------------"
+
 make
  
 echo "--------------------------------------"
-echo "installing OpenPli in $INSTALL_DIR"
+echo "installing OpenPliPC $PKG in $INSTALL_E2DIR"
 echo "--------------------------------------"
+
 sudo make install
 cd ..
  
 echo "--------------------------------------"
-echo "final changes"
+echo "final step: installing E2 conf files"
 echo "--------------------------------------"
 
 # strip binary
-sudo strip $INSTALL_DIR/bin/enigma2
+sudo strip $INSTALL_E2DIR/bin/enigma2
 
 # removing pre-compiled py files
-sudo find $INSTALL_DIR/lib/enigma2/python/ -name "*.py[oc]" -exec rm {} \;
+sudo find $INSTALL_E2DIR/lib/enigma2/python/ -name "*.py[oc]" -exec rm {} \;
 
 # copying needed files
-sudo mkdir -p $INSTALL_DIR/etc/enigma2
-sudo mkdir -p $INSTALL_DIR/etc/tuxbox
-sudo cp share/fonts/* $INSTALL_DIR/share/fonts
-sudo cp -rf etc/* $INSTALL_DIR/etc
+sudo mkdir -p $INSTALL_E2DIR/etc/enigma2
+sudo mkdir -p $INSTALL_E2DIR/etc/tuxbox
+sudo cp share/fonts/* $INSTALL_E2DIR/share/fonts
+sudo cp -rf etc/* $INSTALL_E2DIR/etc
 
 echo ""
 echo "**********************<END>**********************"
